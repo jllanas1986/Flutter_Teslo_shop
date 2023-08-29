61aab2ed318ad56afd02c1358dc2cc642175a60f
Jose Llanas <josellanasmota86@gmail.com>
2023-08-29 13:56:36 +0200

    error management

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -17,13 +17,18 @@ class AuthDataSourceImpl extends AuthDataSource {
   @override
   Future<User> login(String email, String password) async {
     try {
-      final response = await dio.post('/auth/login', data: {'email': email, 'password': password});
+      final response = await dio
+          .post('/auth/login', data: {'email': email, 'password': password});
 
       final user = UserMapper.userJsonToEntity(response.data);
       return user;
-      
+
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) throw WrongCredentials();
+      if (e.type == DioExceptionType.connectionTimeout) throw ConnectionTimeout();
+      throw CustomError('Something wrong happend', 1);
     } catch (e) {
-      throw WrongCredentials();
+      throw CustomError('Something wrong happend', 1);
     }
   }
 
