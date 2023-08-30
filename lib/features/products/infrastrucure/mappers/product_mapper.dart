6e983c9f05522548eb9e802c94b18173acb88a64
Jose Llanas <josellanasmota86@gmail.com>
2023-08-30 19:48:40 +0200

    Riverpod - Products Screen

--- lib/features/products/infrastrucure/mappers/product_mapper.dart
+++ lib/features/products/infrastrucure/mappers/product_mapper.dart
@@ -13,7 +13,7 @@ class ProductMapper {
       sizes: List<String>.from(json['sizes'].map((size) => size)),
       gender: json['gender'],
       tags: List<String>.from(json['tags'].map((tag) => tag)),
-      images: List<String>.from(json['images'].map((String image) =>
+      images: List<String>.from(json['images'].map((image) =>
           image.startsWith('http')
               ? image
               : '${Environment.apiUrl}/files/product/$image')),
