2ce2d8b81db4c48994eab5afd8d2f3bbc684c892
Jose Llanas <josellanasmota86@gmail.com>
2023-09-04 20:37:49 +0200

    Upload the image to the backend

--- lib/features/products/infrastrucure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastrucure/datasources/products_datasource_impl.dart
@@ -14,28 +14,42 @@ class ProductsDatasourceImpl extends ProductsDatasource {
             baseUrl: Environment.apiUrl,
             headers: {'Authorization': 'Bearer $accessToken'}));
 
+  Future<List<String>> _uploadPhotos(List<String> photos) async {
+    final photosToUpload =
+        photos.where((element) => element.contains('/')).toList();
+    final photosToIgnore =
+        photos.where((element) => !element.contains('/')).toList();
+
+    //todo
+    final List<Future<String>> uploadJob = [];
+    final newImages = await Future.wait(uploadJob);
+
+    return [
+      ...photosToIgnore, ...newImages
+    ];
+  }
+
   @override
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike) async {
     try {
       final String? productId = productLike['id'];
       final String method = (productId == null) ? 'POST' : 'PATCH';
-      final String url = (productId == null) ? '/products' : '/products/$productId';
+      final String url =
+          (productId == null) ? '/products' : '/products/$productId';
 
       productLike.remove('id');
+      productLike['images'] = await _uploadPhotos(productLike['images']);
 
-      final response = await dio.request(
-        url,
-        data: productLike, 
-        options: Options(
-          method: method));
+      throw Exception();
+
+      final response = await dio.request(url,
+          data: productLike, options: Options(method: method));
 
       final product = ProductMapper.jsonToEntity(response.data);
       return product;
-
     } catch (e) {
       throw Exception();
     }
-
   }
 
   @override
